8b1dc1fcf759d124cafee057ab888dc38a1b1ecd
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-12-30 22:22:12 -0500

    refactored the return from the fetch api to get more exclusive data

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -10,8 +10,13 @@ const getAllProducts = async (): Promise<any> => {
   const { data } = await fetchApi<ReturnType>({
     query: getAllProductsQuery,
   });
-  //   returning all products
-  return data.products;
+
+  //  Extracting the products from the api into an array of products
+  const products =
+    data.products.edges.map(({ node: product }) => {
+      return product;
+    }) ?? [];
+  return products;
 };
 
 export default getAllProducts;
