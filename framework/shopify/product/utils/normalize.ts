ceffb5b962350f5c52bf35381f852c0c4200dd38
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-12-31 23:55:39 -0500

    normalized the images from the api

--- framework/shopify/product/utils/normalize.ts
+++ framework/shopify/product/utils/normalize.ts
@@ -1,7 +1,25 @@
-import { Product as ShopifyProduct } from "./schema";
+import { ImageEdge, Product as ShopifyProduct } from "./schema";
 
 const normalizeProduct = (productNode: ShopifyProduct): any => {
-  const { id, title: name, handle, vendor, description, ...rest } = productNode;
+  //  Creating function to normalize image Collection
+  function normalizeImages({ edges }: { edges: Array<ImageEdge> }) {
+    return edges.map(({ node: { originalSrc: url, ...rest } }) => {
+      return {
+        url: `/images/${url}`,
+        ...rest,
+      };
+    });
+  }
+
+  const {
+    id,
+    title: name,
+    handle,
+    vendor,
+    description,
+    images: ImageCollection,
+    ...rest
+  } = productNode;
   // Creating a new product that I want to normalize
   const product = {
     id,
@@ -10,6 +28,7 @@ const normalizeProduct = (productNode: ShopifyProduct): any => {
     description,
     path: `/${handle}`,
     slug: handle.replace(/^\/+|\/+$/g, ""),
+    images: normalizeImages(ImageCollection),
     ...rest,
   };
 
