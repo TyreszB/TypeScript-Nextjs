2d57b3f3b5a74dd9bde51769ac1051ae4ce2d524
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-01-02 16:24:50 -0500

    added error handling for config

--- framework/common/next.config.js
+++ framework/common/next.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
 const merge = require("deepmerge");
 
-function withFrameworkConfig(defaultConfig = {}) {
-  const framework = "shopify";
+const ALLOWED_FW = ["shopify"];
 
+function withFrameworkConfig(defaultConfig = {}) {
+  const framework = defaultConfig?.framework?.name;
+
+  if (!framework)
+    throw new Error(
+      "The Api framework is missing, plaese add a valid provider!"
+    );
+
+  if (!ALLOWED_FW.includes(framework)) {
+    throw new Error(
+      `The Api framework: ${framework}, can not be found. Please use allowed frameworks: ${ALLOWED_FW}`
+    );
+  }
   const frameworkNextConfig = require(path.join(
     "../",
     framework,
--- next.config.js
+++ next.config.js
@@ -2,13 +2,16 @@
 
 const { withFrameworkConfig } = require("./framework/common/next.config");
 
+// Added config for shopify in the next config settings
+
 const nextConfig = withFrameworkConfig({
+  framework: {
+    name: "shopify",
+  },
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US",
   },
 });
-
-console.log("next.config.js", JSON.stringify(nextConfig, null, 2));
 
 module.exports = nextConfig;
