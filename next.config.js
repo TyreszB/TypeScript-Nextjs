be6a4095ab7e42289c9db84c5d43cc9e02d4c867
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-01-02 15:48:20 -0500

    created the set up for implmenting shopify store in the config settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const { withFrameworkConfig } = require("./framework/common/next.config");
+
+const nextConfig = withFrameworkConfig({
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  },
+});
+
+console.log("next.config.js", JSON.stringify(nextConfig, null, 2));
+
+module.exports = nextConfig;
