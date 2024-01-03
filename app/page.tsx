9aefee5157ce87cff342f94d2cbbc19775f182ad
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-01-03 16:31:41 -0500

    added grid to product page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import { ProductCard } from "@/components/common/product";
 import getAllProducts from "../framework/shopify/product/get-all-products";
 import { getConfig } from "@/framework/shopify/api/config";
+import { Grid } from "@/components/common/ui";
 
 export default async function Page() {
   const config = getConfig();
   const products = await getAllProducts(config);
 
   return (
-    <div className="fit">
-      {products.map((product) => (
-        <ProductCard product={product} key={product.id} />
-      ))}
-    </div>
+    <>
+      <Grid>
+        {products.map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </Grid>
+    </>
   );
 }
---
+++ components/common/ui/grid/Grid.tsx
@@ -0,0 +1,11 @@
+import React, { FC, ReactNode } from "react";
+
+interface GridProps {
+  children: ReactNode;
+}
+
+const Grid: FC<GridProps> = ({ children }) => {
+  return <div className="root-grid">{children}</div>;
+};
+
+export default Grid;
