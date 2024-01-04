1cf196d928eacabcd6759daa2d8e6be9f94fecba
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-01-04 14:54:08 -0500

    added styling to home product page

--- components/common/product/ProductCard/ProductCard.tsx
+++ components/common/product/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Product } from "@/framework/common/types/products";
 import Link from "next/link";
 import React, { FC } from "react";
 import Image from "next/image";
+import s from "./ProductCard.module.css";
 
 type Props = {
   product: Product;
@@ -11,15 +12,17 @@ const placeholderImage = "/product-image-placeholder.svg";
 
 const ProductCard: FC<Props> = ({ product }) => {
   return (
-    <Link href={`/products/${product.slug}`}>
-      <div>
-        <h3>{product!.name}</h3>
-        <span>14 $</span>
+    <Link href={`/products/${product.slug}`} className={s.root}>
+      <div className={s.productTag}>
+        <h3 className={s.productTitle}>
+          <span>{product!.name}</span>
+        </h3>
+        <span className={s.productPrice}>14 $</span>
       </div>
       {product.images && (
         <Image
-          alt={product.name ?? "Product imaage"}
-          src={placeholderImage}
+          alt={product.name ?? "Product image"}
+          src={product.images[0].url ?? placeholderImage}
           height={540}
           width={540}
           quality="85"
--- components/common/ui/grid/Grid.tsx
+++ components/common/ui/grid/Grid.tsx
@@ -1,12 +1,19 @@
 import React, { FC, ReactNode } from "react";
 import s from "./Grid.module.css";
+import cn from "classnames";
 
 interface GridProps {
-  children: ReactNode;
+  children: ReactNode[];
+  layout?: "A" | "B";
 }
 
-const Grid: FC<GridProps> = ({ children }) => {
-  return <div className={s.root}>{children}</div>;
+const Grid: FC<GridProps> = ({ children, layout = "A" }) => {
+  const rootClassName = cn(s.root, {
+    [s.layoutA]: layout === "A",
+    [s.layoutB]: layout === "B",
+  });
+
+  return <div className={rootClassName}>{children}</div>;
 };
 
 export default Grid;
