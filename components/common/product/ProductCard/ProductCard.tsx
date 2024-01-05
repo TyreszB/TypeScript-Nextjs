7b98130fcda6f3b02803cc15a180258dc41c2b0d
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-01-04 22:52:48 -0500

    added bg color to products

--- components/common/product/ProductCard/ProductCard.tsx
+++ components/common/product/ProductCard/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { Product } from "@/framework/common/types/products";
 import Link from "next/link";
 import React, { FC } from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import s from "./ProductCard.module.css";
 
 type Props = {
@@ -12,23 +12,26 @@ const placeholderImage = "/product-image-placeholder.svg";
 
 const ProductCard: FC<Props> = ({ product }) => {
   return (
-    <Link href={`/products/${product.slug}`} className={s.root}>
-      <div className={s.productTag}>
-        <h3 className={s.productTitle}>
-          <span>{product!.name}</span>
-        </h3>
-        <span className={s.productPrice}>14 $</span>
-      </div>
-      {product.images && (
-        <Image
-          alt={product.name ?? "Product image"}
-          src={product.images[0].url ?? placeholderImage}
-          height={540}
-          width={540}
-          quality="85"
-          layout="responsive"
-        />
-      )}
+    <Link legacyBehavior href={`/products/${product.slug}`}>
+      <a className={s.root}>
+        <div className={s.productBg}></div>
+        <div className={s.productTag}>
+          <h3 className={s.productTitle}>
+            <span>{product!.name}</span>
+          </h3>
+          <span className={s.productPrice}>14 $</span>
+        </div>
+        {product.images && (
+          <Image
+            alt={product.name ?? "Product image"}
+            src={product.images[0].url ?? placeholderImage}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )}
+      </a>
     </Link>
   );
 };
